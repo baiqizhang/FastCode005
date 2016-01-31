3f0a44771b8a8449c15eea7f4802107834a057d8
Baiqi Zhang <baiqiz@GHC44.GHC.ANDREW.CMU.EDU>
2016-01-31 18:13:30 -0500

    SIMD

--- matrix_mul/omp/matrix_mul.cpp
+++ matrix_mul/omp/matrix_mul.cpp
@@ -28,7 +28,7 @@ namespace omp
     void
     matrix_multiplication(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, unsigned int sq_dimension )
     {
-        //Test Case 5	76.3589 milliseconds
+        //Test Case 5	68.302 milliseconds
         float *A, *B_t;
         unsigned int n = sq_dimension, N;
         unsigned int i, j, k;
@@ -46,7 +46,7 @@ namespace omp
         B_t = (float*)calloc(N*N, sizeof(float));
         for (i = 0; i < n; i++)
             for(j = 0; j < n; j++){
-                if (N != n) //not mul of 4, fill A
+                if (N != n) //n is not mul of 4, fill A
                     A[i * N + j] = sq_matrix_1[i * n + j];
                 B_t[i * N + j] = sq_matrix_2[j * n + i];
             }
@@ -58,12 +58,15 @@ namespace omp
     shared(sq_matrix_result,A,B_t)
         for (i = 0; i < n; i++)
             for (j = 0; j < n; j++) {
+                // SIMD
                 __m128 sum = _mm_setzero_ps();
                 
+                // mul and sum 4 pairs of float in 4 instructions
                 for (k = 0; k < n; k += 4) {
                     sum = _mm_add_ps(sum, _mm_mul_ps(_mm_load_ps(&A[i * N + k]),
                                                      _mm_load_ps(&B_t[j * N + k])));
                 }
+                // store __m128 to float array, sum up and save
                 _mm_store_ps(temp, sum);
                 result = temp[0] + temp[1] + temp[2] + temp[3];
                 sq_matrix_result[i*n + j] = result;
