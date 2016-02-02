6dfee8b14316b205ef229545595daa9734b20534
Baiqi Zhang <baiqiz@GHC44.GHC.ANDREW.CMU.EDU>
2016-02-02 00:06:02 -0500

    remove junk

--- matrix_mul/omp/matrix_mul.cpp
+++ matrix_mul/omp/matrix_mul.cpp
@@ -26,6 +26,8 @@
 
 namespace omp
 {
+    __m128 t[1000];
+
     void
     matrix_multiplication(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, unsigned int sq_dimension )
     {
@@ -34,7 +36,7 @@ namespace omp
         unsigned int n = sq_dimension, N;
         unsigned int i, j, k;
         
-        if (n<100){
+        if (n<50){
 #pragma omp parallel for
             for (unsigned int i = 0; i < sq_dimension; i++)
                 for(unsigned int j = 0; j < sq_dimension; j++)
@@ -69,7 +71,6 @@ namespace omp
         
         // Matrix Mul
         float temp[8], result;
-        __m128 t[1000];
         __m128 sum;
         unsigned int ind;
         
@@ -85,12 +86,11 @@ namespace omp
             }
             for (j = 0; j < n; j++) {
                 // SIMD
-                sum = _mm_setzero_ps();
                 
                 // mul and sum 4 pairs of float in 4 instructions
-                for (ind = 0, k = 0; k < n; k += 4, ind++) {
-                    sum = _mm_add_ps(sum, _mm_mul_ps(t[ind],_mm_load_ps(&B_t[j * N + k]) ));
-                }
+                for (ind = 0, k = 0; k < n; k += 4, ind++)
+                    sum = _mm_mul_ps(t[ind],_mm_load_ps(&B_t[j * N + k]) );
+
                 // store __m128 to float array, sum up and save
                 _mm_store_ps(temp, sum);
                 result = temp[0] + temp[1] + temp[2] + temp[3];
