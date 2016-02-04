08228c93b965c096ce256d7cfa1735bd76fccc39
Baiqi Zhang <baiqiz@GHC44.GHC.ANDREW.CMU.EDU>
2016-02-03 20:56:29 -0500

    test

--- matrix_mul/omp/matrix_mul.cpp
+++ matrix_mul/omp/matrix_mul.cpp
@@ -71,7 +71,7 @@ namespace omp
         __m128 t[1000];
         __m128 sum;
         unsigned int ind;
-        unsigned int step = 13;
+        unsigned int step = 9;
         
 #pragma omp parallel for \
 private(k,ind,sum,t,temp,result) \
