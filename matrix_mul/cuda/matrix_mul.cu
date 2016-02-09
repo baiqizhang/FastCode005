75b54acddac56f35c1af58e88702203fef0cbdb2
shenggul <lumig242@gmail.com>
2016-02-09 02:28:08 -0500

    Add tile to matrix multiply

--- matrix_mul/cuda/matrix_mul.cu
+++ matrix_mul/cuda/matrix_mul.cu
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2011  Abhinav Jauhri (abhinav.jauhri@gmail.com), Carnegie Mellon University - Silicon Valley 
+    Copyright (C) 2011  Abhinav Jauhri (abhinav.jauhri@gmail.com), Carnegie Mellon UniversithreadIdx.y - Silicon Valley 
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -7,19 +7,19 @@
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    but WITHOUT ANY WARRANthreadIdx.y; without even the implied warranthreadIdx.y of
+    MERCHANTABILIthreadIdx.y or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
+#define TILE_WIDTH 32
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include "matrix_mul.h"
-#define TILE_WIDTH 2
+//#define TILE_WIDTH 2
 
 namespace cuda
 {
@@ -28,40 +28,91 @@ namespace cuda
  //  matrix_mul_kernel(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, int sq_dimension)
  //  {
     
- //    int tx = threadIdx.x;
- //    int ty = threadIdx.y;
+ //    int threadIdx.x = threadIdx.x;
+ //    int threadIdx.y = threadIdx.y;
     
  //    float sum = 0.0f;
     
  //    for(int k = 0; k < sq_dimension; k++)
  //      {
-	// sum += sq_matrix_1[ty*sq_dimension + k] * sq_matrix_2[k*sq_dimension + tx];
+	// sum += sq_matrix_1[threadIdx.y*sq_dimension + k] * sq_matrix_2[k*sq_dimension + threadIdx.x];
  //      }
- //    sq_matrix_result[ty*sq_dimension + tx] = sum;
+ //    sq_matrix_result[threadIdx.y*sq_dimension + threadIdx.x] = sum;
     
  //  }
 
 
   // rewrote the kernel for easier modification - Vincent
-  __global__ void matrix_mul_kernel(float *A, float *B, float *C, int d) {
+  //__global__ void matrix_mul_kernel(float *A, float *B, float *C, int d) {
+   
+  //  __shared__ float A_tile[blockDim.y][blockDim.x];
+  //  __shared__ float B_tile[blockDim.y][blockDim.x];
     
+    
+  //  float temp = 0.0;
+  //  for(int index = 0; index < (d/blockDim.x - 1); index++){
+  //      int row = blockIdx.y * blockDim.y + threadIdx.y;
+  //      int col = index * blockDim.x + threadIdx.x;
+  //      *A_tile[threadIdx.y][threadIdx.x] = A[row][col];
+  //      *B_tile[threadIdx.x][threadIdx.y] = B[col][row];
+  //      __syncthreads();
+  //      for(int k = 0; k < blockDim.x; k++){
+  //          temp += A_tile[threadIdx.y][k] * B_tile[k][threadIdx.x];
+  //      }
+  //      __syncthreads();
+  //  }
+  //  C[blockIdx.y * blockDim.y + threadIdx.y][blockIdx.x * blockDim.x + threadIdx.x] = temp;
+
     // initialize row and column by index in the blocks
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    //int row = blockIdx.y * blockDim.y + threadIdx.y;
+    //int col = blockIdx.x * blockDim.x + threadIdx.x;
 
     // check if out of matrix boundary
-    if (row < d && col < d) {
-      float tmp = 0.0;
+    //if (row < d && col < d) {
+    //  float tmp = 0.0;
 
       //loop unrolling
-      #pragma unroll
-      for (int i = 0; i < d; i++) {
-        tmp += A[row * d + i] * B[i * d + col];
-      }
-      C[row * d + col] = tmp;
-    }
-  }
+    //  #pragma unroll
+    //  for (int i = 0; i < d; i++) {
+    //    tmp += A[row * d + i] * B[i * d + col];
+    //  }
+    //  C[row * d + col] = tmp;
+    //}
+  //}
   
+  // Compute C = A * B
+  __global__ void matrixMultiply(float * A, float * B, float * C, int d){
+    __shared__ float A_tile[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float B_tile[TILE_WIDTH][TILE_WIDTH];
+    int row = blockIdx.y * TILE_WIDTH + threadIdx.y, col = blockIdx.x * TILE_WIDTH + threadIdx.x;
+    float sum = 0;
+
+    #pragma unroll
+    for (int m = 0; m < (d-1)/TILE_WIDTH+1; ++m) {
+      if (row < d && m*TILE_WIDTH+threadIdx.x < d){
+        A_tile[threadIdx.y][threadIdx.x] = A[row*d + m*TILE_WIDTH+threadIdx.x];
+      }
+      else{
+        A_tile[threadIdx.y][threadIdx.x] = 0;
+      }
+      if (col < d && m*TILE_WIDTH+threadIdx.y < d){
+        B_tile[threadIdx.y][threadIdx.x] = B[(m*TILE_WIDTH+threadIdx.y)*d+col];
+      }
+      else{
+        B_tile[threadIdx.y][threadIdx.x] = 0;
+      }
+
+      __syncthreads();
+      #pragma unroll
+      for (int k = 0; k < TILE_WIDTH; ++k)
+        sum += A_tile[threadIdx.y][k] * B_tile[k][threadIdx.x];
+      __syncthreads();
+    }
+    if (row < d && col < d)
+      C[row*d + col] = sum;
+  }
+
+
   void 
   matrix_multiplication(float *sq_matrix_1, float *sq_matrix_2, float *sq_matrix_result, unsigned int sq_dimension)
   {
@@ -89,11 +140,15 @@ namespace cuda
     // matrix_mul_kernel<<<dimGrid, dimBlock, dimBlock.x * dimBlock.x * sizeof(float)>>>(sq_matrix_1_d, sq_matrix_2_d, sq_matrix_result_d, sq_dimension);
 
     // increase block dimension
-    int blockDimension = 32;
-    dim3 dimBlock(blockDimension, blockDimension);
-    dim3 dimGrid(ceil(double(sq_dimension)/double(dimBlock.x)), ceil(double(sq_dimension)/double(dimBlock.y)));
-    matrix_mul_kernel<<<dimGrid, dimBlock>>>(sq_matrix_1_d, sq_matrix_2_d, sq_matrix_result_d, sq_dimension);
-    
+    //int blockDimension = 32;
+    //dim3 dimBlock(blockDimension, blockDimension);
+    //dim3 dimGrid(ceil(double(sq_dimension)/double(dimBlock.x)), ceil(double(sq_dimension)/double(dimBlock.y)));
+    //matrix_mul_kernel<<<dimGrid, dimBlock>>>(sq_matrix_1_d, sq_matrix_2_d, sq_matrix_result_d, sq_dimension);
+    dim3 dimGrid((sq_dimension-1)/TILE_WIDTH+1, (sq_dimension-1)/TILE_WIDTH+1, 1);
+    dim3 dimBlock(TILE_WIDTH, TILE_WIDTH, 1);
+    matrixMultiply<<<dimGrid, dimBlock>>>(sq_matrix_1_d, sq_matrix_2_d, sq_matrix_result_d, sq_dimension);
+    cudaThreadSynchronize();
+
     /***************************************************
    3rd Part: Transfer result from device to host 
     ****************************************************/
