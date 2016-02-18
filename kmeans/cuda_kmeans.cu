65b346de7faa37b1cba5b3178e104a69e58a01f7
Sheng Chieh Su <shengchs@ghc38.ghc.andrew.cmu.edu>
2016-02-17 20:04:24 -0500

    128

--- kmeans/cuda_kmeans.cu
+++ kmeans/cuda_kmeans.cu
@@ -311,10 +311,7 @@ float** cuda_kmeans(float **objects,      /* in: [numObjs][numCoords] */
     //  two, and it *must* be no larger than the number of bits that will
     //  fit into an unsigned char, the type used to keep track of membership
     //  changes in the kernel.
-    const unsigned int numThreadsPerClusterBlock = 256;
-    if (numObjs == 191681) {
-        numThreadsPerClusterBlock = 1024;
-    }
+    const unsigned int numThreadsPerClusterBlock = 128;
     const unsigned int numClusterBlocks =
         (numObjs + numThreadsPerClusterBlock - 1) / numThreadsPerClusterBlock;
     const unsigned int clusterBlockSharedDataSize =
