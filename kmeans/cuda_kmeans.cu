faa91854d9bf118fd48173af7cafbba9f3b50666
Sheng Chieh Su <shengchs@ghc38.ghc.andrew.cmu.edu>
2016-02-19 21:36:55 -0500

    delta

--- kmeans/cuda_kmeans.cu
+++ kmeans/cuda_kmeans.cu
@@ -219,21 +219,21 @@ void compute_delta(int *deviceIntermediates,
     }
 
     // for (unsigned int s = numIntermediates2 / 2; s > 32; s >>= 1) {
-    //     if (threadIdx.x < s) {
-    //         intermediates[threadIdx.x] += intermediates[threadIdx.x + s];
+    //     if (tid < s) {
+    //         intermediates[tid] += intermediates[tid + s];
     //     }
     //     __syncthreads();
     // }
 
     //  // Unrolling warp
-    // if (threadIdx.x < 32){
+    // if (tid < 32){
     //     volatile unsigned int* vmem = intermediates;
-    //     vmem[threadIdx.x] += vmem[threadIdx.x+32];
-    //     vmem[threadIdx.x] += vmem[threadIdx.x+16];
-    //     vmem[threadIdx.x] += vmem[threadIdx.x+8];
-    //     vmem[threadIdx.x] += vmem[threadIdx.x+4];
-    //     vmem[threadIdx.x] += vmem[threadIdx.x+2];
-    //     vmem[threadIdx.x] += vmem[threadIdx.x+1];
+    //     vmem[tid] += vmem[tid+32];
+    //     vmem[tid] += vmem[tid+16];
+    //     vmem[tid] += vmem[tid+8];
+    //     vmem[tid] += vmem[tid+4];
+    //     vmem[tid] += vmem[tid+2];
+    //     vmem[tid] += vmem[tid+1];
     // }
 
     if (tid == 0) {
