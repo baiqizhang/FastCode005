2c1a9fb0ac32eca6f6eca0a4c213e737c743f617
Sheng Chieh Su <shengchs@ghc38.ghc.andrew.cmu.edu>
2016-02-19 20:57:56 -0500

    no delta

--- kmeans/cuda_kmeans.cu
+++ kmeans/cuda_kmeans.cu
@@ -208,23 +208,11 @@ void compute_delta(int *deviceIntermediates,
     __syncthreads();
 
     //  numIntermediates2 *must* be a power of two!
-    for (unsigned int s = numIntermediates2 / 2; s > 32; s >>= 1) {
+    for (unsigned int s = numIntermediates2 / 2; s > 0; s >>= 1) {
         if (threadIdx.x < s) {
             intermediates[threadIdx.x] += intermediates[threadIdx.x + s];
         }
         __syncthreads();
-    }
-
-
-    // Unrolling warp
-    if(threadIdx.x < 32){
-        volatile unsigned int* vmem = intermediates;
-        vmem[threadIdx.x] += vmem[threadIdx.x+32];
-        vmem[threadIdx.x] += vmem[threadIdx.x+16];
-        vmem[threadIdx.x] += vmem[threadIdx.x+8];
-        vmem[threadIdx.x] += vmem[threadIdx.x+4];
-        vmem[threadIdx.x] += vmem[threadIdx.x+2];
-        vmem[threadIdx.x] += vmem[threadIdx.x+1];
     }
 
     if (threadIdx.x == 0) {
